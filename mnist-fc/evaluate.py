5d32b68094bc8b6fd1b472aebb938b99c7099a33
Shaheera90 <shaheera@altafuturis.com>
2022-07-08 00:41:03 +0530

    evaluation of the MNIST model

---
+++ mnist-fc/evaluate.py
@@ -0,0 +1,32 @@
+from keras.datasets import mnist 
+from keras.models import load_model
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+model_h = load_model("models/mnist.h5")
+
+score = model.evaluate(X_test, Y_test)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+# The predict_classes function outputs the highest probability class
+# according to the trained classifier for each input example.
+predicted_classes = model.predict_classes(X_test)
+# Check which items we got right / wrong
+correct_indices = np.nonzero(predicted_classes == y_test)[0]
+incorrect_indices = np.nonzero(predicted_classes != y_test)[0]
+
+plt.figure()
+for i, correct in enumerate(correct_indices[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_test[correct].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[correct], y_test[correct]))
+    
+plt.tight_layout()
+    
+plt.figure()
+for i, incorrect in enumerate(incorrect_indices[:9]):
+    plt.subplot(3,3,i+1)
+    plt.imshow(X_test[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title("Predicted {}, Class {}".format(predicted_classes[incorrect], y_test[incorrect]))
+    
+plt.tight_layout()
